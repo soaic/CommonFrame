467e86a06d586626ab8a25afa66516cdf85d71e7
soaic <xiaosai_vip@163.com>
2016-10-23 11:39:32 +0800

    Initial commit

---
+++ app/src/main/java/com/commonframe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.commonframe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
